b5acf96a8741ebc9e2eb7a6d1608cc91d3787593
chintas1 <chintas1@tcnj.edu>
2016-05-05 13:22:00 +0000

    nitrous-backup at Thu May  5 13:22:00 UTC 2016

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,20 @@
+require 'prime'
 # Implement your procedural solution here!
+def largest_palindrome_product
+  998001.downto(10000) do |num|
+    (return num) if (is_palindrome?(num) && !Prime.prime?(num))
+  end
+  nil
+end
+
+def is_palindrome?(num)
+  num = num.to_s
+  start = 0
+  last = -1
+  until (start == (num.size/2))
+    return false if(num[start]!=num[last])
+    start +=1
+    last -= 1
+  end
+  return true
+end
